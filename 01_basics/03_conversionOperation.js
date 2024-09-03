75f89170c1f387276e4e852b159a2357ae42b866
Sudhanshu Gupta <125302083+Sudhanshu7777@users.noreply.github.com>
2024-09-03 10:05:25 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,22 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************** Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+
+let str1 = "hello"
+let str2 = " sudhanshu"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
 
