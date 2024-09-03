b6b57fb265099d80b64a4390eb0b4941265c35c3
Sudhanshu Gupta <125302083+Sudhanshu7777@users.noreply.github.com>
2024-09-03 17:46:37 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 345633526632652n
+
+
+
+// Refrence (Non Primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"];
+let myObj ={
+    name: "sudhanshu",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");    
+}
+
+console.log(typeof heroes);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
